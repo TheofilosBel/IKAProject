4de632e614ddecb7dae036c4ccdce2c46a156c85
TheofilosBel <theofilos_mp@outlook.com.gr>
2017-11-25 19:31:50 +0200

    first page layout

--- www/index.php
+++ www/index.php
@@ -1,9 +1,42 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>My First PHP Web Page</title>
+        <link rel="stylesheet" type="text/css" href="CSS/general_layout.css">
+        <link rel="stylesheet" type="text/css" href="CSS/index.css">
+        <title>IKA-Αρχική Σελίδα</title>
     </head>
+
+
     <body>
-        <?php print('Hello Worldaaaaaaa!'); ?>
-    </body>
-</html>
+
+        <header>
+            <p> THIS IS THE HEADER </p>
+        </header>  <!-- header -->
+
+        <main>
+            
+            <div class="container">
+
+                <div class="main-opts">
+                    <div class="box a">A</div>
+                    <div class="box b">B</div>
+                    <div class="box c">C</div>
+                    <div class="box d">D</div>
+                </div>  <!--div child 1-->
+
+                <div class="secondary-opts">
+                    <div class="box e">D</div>
+                    <div class="box f">E</div>
+                    <div class="box g">F</div>
+                </div>  <!--div child 2-->
+
+
+            </div>  <!-- div -->
+        </main>  <!-- main -->
+
+        <footer>
+            <p> THIS IS THE FOOTER </p>
+        </footer>  <!-- footer -->
+
+    </body> <!-- body -->
+</html>  

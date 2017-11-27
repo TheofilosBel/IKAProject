c7b6d399c2f809609f3737bf0338661ab65a4968
orestig <orestijuve@gmail.com>
2017-11-27 18:51:38 +0200

    Added images to front page

---
+++ www/includes/header.php
@@ -0,0 +1,13 @@
+<header>
+    
+    <!-- LOGO -->
+
+    <ul class="nav_menu">
+        <li><a href="#"><img src="images/home.png" height="20" width="20"><h4>Αρχική</h4></a></li>
+        <li><a href="#"><img src="images/search.png" height="20" width="20"><h4>Αναζήτηση</h4></a></li>
+        <li><a href="#"><img src="images/menu.png" height="20" width="20"><h4>Μενού</h4></a></li>
+        <li><a href="#"><img src="images/globe.png" height="20" width="20"><h4>Γλώσσα</h4></a></li>
+        <li><a href="#"><img src="images/login.png" height="20" width="20"><h4>Είσοδος</h4></a></li>
+    </ul>
+
+</header>
--- www/index.php
+++ www/index.php
@@ -1,28 +1,28 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <link rel="stylesheet" type="text/css" href="CSS/general_layout.css">
-        <link rel="stylesheet" type="text/css" href="CSS/index.css">
+        <link rel="stylesheet" type="text/css" href="css/general_layout.css">
+        <link rel="stylesheet" type="text/css" href="css/index.css">
         <meta charset="utf8">
         <title>ΙΚΑ - Αρχική Σελίδα</title>
     </head>
 
     <body>
-        <?php include 'Includes/header.php'; ?>  
+        <?php include 'includes/header.php'; ?>  
 
         <main>
             <div class="container">
                 <div class="main-opts">
-                    <a href='a.php' class="box asfal">Ασφαλισμένοι</a>
-                    <a href="#" class="box synta">Συνταξιούχοι</a>
-                    <a href="#" class="box ergo">Εργοδότες</a>
-                    <a href="#" class="box foreis">Φορείς</a>
+                    <a href='a.php' class="box asfal"><img src="images/asf.png" height="70" width="70">Ασφαλισμένοι</a>
+                    <a href="#" class="box synta"><img src="images/synt.png" height="70" width="70">Συνταξιούχοι</a>
+                    <a href="#" class="box ergo"><img src="images/erg.png" height="70" width="70">Εργοδότες</a>
+                    <a href="#" class="box foreis"><img src="images/foreis.png" height="70" width="70">Φορείς</a>
                 </div>  <!--div child 1-->
 
                 <div class="secondary-opts">
-                    <a href="#" class="box faqs">Συχνές Ερωτήσεις - <br> FAQs</a>
-                    <a href="#" class="box forum">Φόρουμ</a>
-                    <a href="#" class="box epik">Επικοινωνία</a>
+                    <a href="#" class="box faqs"><img src="images/faqs.png" height="70" width="70">Συχνές Ερωτήσεις - FAQs</a>
+                    <a href="#" class="box forum"><img src="images/forum.png" height="70" width="70">Φόρουμ</a>
+                    <a href="#" class="box epik"><img src="images/contact.png" height="70" width="70">Επικοινωνία</a>
                 </div>  <!--div child 2-->
 
 

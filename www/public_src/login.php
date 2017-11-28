0df5e1aa0b068fd5580e4ffb250b5a86c4487d41
orestig <orestijuve@gmail.com>
2017-11-28 18:52:31 +0200

    Added auto login

--- www/public_src/login.php
+++ www/public_src/login.php
@@ -16,17 +16,23 @@
         <div class="container">
             <h1>Σύνδεση στο ΙΚΑ</h1>
             <div class="login-box">
-                <form action="#">
-                    <label for="name">Ψευδώνυμο Χρήστη</label>
+                <form action="#" method="post">
+                    <label for="name">Ψευδώνυμο χρήστη</label>
                     <input type="text" id="name" name="username" autofocus>
 
                     <label for="pass">Συνθηματικό</label>
                     <input type="text" id="pass" name="password">
                     <a href="#">Ξεχάσατε το συνθηματικό σας;</a>
 
+                    <div class="keep-login">
+                        <input type="checkbox" id="keep-login" name="keep-login"/>
+                        <label for="keep-login">Αυτόματη σύνδεση</label> 
+                    </div>
+
                     <input type="submit" value="Είσοδος">
                 </form> 
             </div>
+
             <div class="signup-box">
                 <p>Είστε νέος χρήστης; <a href="#">Εγγραφείτε</a>.</p>
             </div>

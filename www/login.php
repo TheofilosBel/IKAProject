8ce8a7d7766508351ef3d01c9715bf620db0928a
orestig <orestijuve@gmail.com>
2017-11-28 15:12:28 +0200

    h

--- www/login.php
+++ www/login.php
@@ -1,27 +1,27 @@
 <!DOCTYPE html>
 <html>
     <head>
-	   <link rel="stylesheet" type="text/css" href="css/general_layout.css">
+        <link rel="stylesheet" type="text/css" href="css/general_layout.css">
         <meta charset="utf8">
-	   <title>ΙΚΑ - Είσοδος Χρήστη</title>
+        <title>ΙΚΑ - Είσοδος Χρήστη</title>
     </head>
 
     <body>
 
     <!-- Include the header -->
-	<?php include './includes/header.php' ?>
+    <?php include './includes/header.php' ?>
 
-	<main>
-		<h3> Σύνδεση στο ΙΚΑ </h3>
-		
-
-
-	</main>  <!-- main -->
+    <main>
+        <h3> Σύνδεση στο ΙΚΑ </h3>
+        
 
 
-	<!-- Include the footer -->
-	<?php include 'includes/footer.php' ?>
+    </main>  <!-- main -->
 
-	</body>
+
+    <!-- Include the footer -->
+    <?php include 'includes/footer.php' ?>
+
+    </body>
 
 </html>

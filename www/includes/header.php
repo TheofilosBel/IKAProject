b5bbea3af2f5e5b8ad2b7ebe9b93f7f87ae375d1
Theofilos <theofilos_mp@outlook.com.gr>
2017-11-28 13:40:11 +0200

    bar fixx

--- www/includes/header.php
+++ www/includes/header.php
@@ -1,12 +1,12 @@
 <header>
     <!-- LOGO -->
-    <img src="images/logo.png" height="75" width="75">
+    <img src="images/logo.png" height="70" width="70">
 
     <ul class="nav_menu">
         <li><a href="./"><img src="images/home.png" height="20" width="20"><p>Αρχική</p></a></li>
         <li><a href="#"><img src="images/search.png" height="20" width="20"><p>Αναζήτηση</p></a></li>
         <li><a href="#"><img src="images/menu.png" height="20" width="20"><p>Μενού</p></a></li>
         <li><a href="#"><img src="images/globe.png" height="20" width="20"><p>Γλώσσα</p></a></li>
-        <li><a href="./login.php"><img src="images/login.png" height="20" width="20"><p>Είσοδος</p></a></li>
+        <li><a href="./login.php"><img src="images/login.png" height="20" width="20"><p>Είσοδος/Εγγραφή</p></a></li>
     </ul>
 </header>

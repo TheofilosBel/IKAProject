696693cab8ca3dae87fc8af717b65593597fd9f4
Theofilos <theofilos_mp@outlook.com.gr>
2017-11-28 12:30:31 +0200

    bar fix

--- www/includes/header.php
+++ www/includes/header.php
@@ -1,13 +1,13 @@
 <header>
     <!-- LOGO -->
-    <img src="images/logo.png" height="50" width="50">
+    <img src="images/logo.png" height="75" width="75">
 
     <ul class="nav_menu">
-        <li><a href="#"><img src="images/home.png" height="20" width="20"><h4>Αρχική</h4></a></li>
-        <li><a href="#"><img src="images/search.png" height="20" width="20"><h4>Αναζήτηση</h4></a></li>
-        <li><a href="#"><img src="images/menu.png" height="20" width="20"><h4>Μενού</h4></a></li>
-        <li><a href="#"><img src="images/globe.png" height="20" width="20"><h4>Γλώσσα</h4></a></li>
-        <li><a href="#"><img src="images/login.png" height="20" width="20"><h4>Είσοδος</h4></a></li>
+        <li><a href="#"><img src="images/home.png" height="20" width="20"><p>Αρχική</p></a></li>
+        <li><a href="#"><img src="images/search.png" height="20" width="20"><p>Αναζήτηση</p></a></li>
+        <li><a href="#"><img src="images/menu.png" height="20" width="20"><p>Μενού</p></a></li>
+        <li><a href="#"><img src="images/globe.png" height="20" width="20"><p>Γλώσσα</p></a></li>
+        <li><a href="#"><img src="images/login.png" height="20" width="20"><p>Είσοδος</p></a></li>
     </ul>
 
 </header>

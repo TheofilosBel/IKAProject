2c790c8dc9f22b1345ec8cab698538bf90a4f5a6
orestig <orestijuve@gmail.com>
2018-01-13 21:56:59 +0200

    pension calc done

--- www/public_src/pension_calc.php
+++ www/public_src/pension_calc.php
@@ -12,6 +12,7 @@ require_once(__ROOT__."/resources/config.php");
         <link rel="stylesheet" type="text/css" href="css/tab_decoration.css">
         <link rel="stylesheet" type="text/css" href="css/pension_calc.css">
         <script src="./js/tab_handler.js"></script>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
     </head>
 
 
@@ -34,9 +35,8 @@ require_once(__ROOT__."/resources/config.php");
                         <span>Συνταξιοδοτικός Φορέας</span>
                         <div class="select-style">
                             <select name="pension-carrier">
-                                <option value="IKA">IKA</option>
-                                <option value="PIKA">PIKA</option>
-                                <option value="MIKA">MIKA</option>
+                                <option value="IKA">ΙΚΑ</option>
+                                <option value="PIKA">ΙΚΑ-ΕΤΑΜ</option>
                             </select> <!-- End of Secelct -->
                         </div>
 
@@ -44,7 +44,7 @@ require_once(__ROOT__."/resources/config.php");
                         <div class="select-style">
                             <select name="pension-type">
                                 <option value="Simple">Απλή</option>
-                                <option value="Extra">Με απόλα</option>
+                                <option value="Extra">Αναπηρική</option>
                             </select> <!-- End of Secelct -->
                         </div>
 
@@ -56,41 +56,126 @@ require_once(__ROOT__."/resources/config.php");
                     <div class="tool-info-container">
                         <span class="align-start">Στοιχεία Τελευταίας <br> Δεκαετίας</span>
 
-                        <div class="tool-table-container">
-                            <span>Έτος</span>
-                            <span>Αποδοχές</span>
-                            <span>Ημέρες Εργασίας</span>
-                            <?php
-                                $curent_year = date("Y") - 1;
-                                for ($y = 0; $y < 10; $y++) {
-                                    echo '<p>'.($curent_year-$y).'</p>'.'<input class="apodoxes"/><input class="hmeres"/>';
-                                }
-                            ?>
-                        </div>  <!-- End of the table Div -->
+                        <form id="myform">
+                            <div class="tool-table-container">
+                                <span>Έτος</span>
+                                <span>Αποδοχές</span>
+                                <span>Ημέρες Εργασίας</span>
+                                <?php
+                                    $curent_year = date("Y") - 1;
+                                    for ($y = 0; $y < 10; $y++) {
+                                        echo '<p>'.($curent_year-$y).'</p>'.'<input class="apodoxes"/><input class="hmeres"/>';
+                                    }
+                                ?>
+                            </div>  <!-- End of the table Div -->
+                            <br><span class="help-block"></span><br>
 
-                        <div class="align-container">
-                            <button type="button" class="calculate" onclick="compute()">Υπολογισμός</button>
-                            <button type="button" class="clean">Καθαρισμός</button>
-                        </div>  <!-- End of the Align Div -->
+                            <div class="align-container">
+                                <button type="button" id="calculate" class="calculate" onclick="compute()">Υπολογισμός</button>
+                                <button type="button" id="clean" class="clean">Καθαρισμός</button>
+                            </div>  <!-- End of the Align Div -->
+                        </form>
                     </div>  <!--End of The General Info Div -->
                 </div> <!-- The Main Tool tab-->
 
+                <!-- The Modal -->
+                <div id="myModal" class="modal">
+                    <div class="modal-content">
+                        <span class="close">&times;</span>
+                        <p>Το ποσό της σύνταξης σας είναι <span id="amount" style="font-weight: bold;"></span> ευρώ.</p>
+                    </div>
+                </div>
+
                 <script type="text/javascript">
                     function compute() {
+                        var str_input;
+                        var total_num_of_days;
+                        var flag;
+
                         /* Get num_of_days value */
-                        var total_num_of_days = document.getElementsByClassName("num-of-days").value;
+                        if (document.getElementsByClassName("num-of-days")[0].value)
+                            total_num_of_days = parseInt(document.getElementsByClassName("num-of-days")[0].value);
 
                         /* Get the data from the form */
                         var total_apodoxes = 0;
                         var total_hmeres = 0;
+
                         for (i = 0; i < 10; i++) {
-                            total_apodoxes += document.getElementsByClassName("apodoxes")[i].value;
-                            total_hmeres += document.getElementsByClassName("hmeres")[i].value;
+                            flag = 0;
+
+                            if (document.getElementsByClassName("apodoxes")[i].value) {
+                                total_apodoxes += parseInt(document.getElementsByClassName("apodoxes")[i].value);
+                                flag = 1;
+                            }
+
+                            if (document.getElementsByClassName("hmeres")[i].value) {
+                                total_hmeres += parseInt(document.getElementsByClassName("hmeres")[i].value);
+                                if (flag == 0) {
+                                    document.getElementsByClassName("help-block")[0].innerHTML = "Πρέπει να συμπληρώσετε\
+                                    και τα δύο πεδία σε κάθε γραμμή.";
+                                    return;
+                                }
+                            }
+                            else {
+                                if (flag == 1) {
+                                    document.getElementsByClassName("help-block")[0].innerHTML = "Πρέπει να συμπληρώσετε\
+                                    και τα δύο πεδία σε κάθε γραμμή.";
+                                    return;
+                                }
+                            }
                         }
 
-                        document.write(total_num_of_days);
-                        document.write(total_apodoxes);
-                        document.write(total_hmeres);
+                        /* Error check total days */
+                        if (total_num_of_days != total_hmeres) {
+                            document.getElementsByClassName("help-block")[0].innerHTML = "Το Σύνολο Ημερών Εργασίας\
+                            δεν ισούται με το άθροισμα των ημερών εργασίας που έχετε εισάγει.";
+                            return;
+                        }
+
+                        /* Compute the pension amount */
+                        var total_amount = total_apodoxes * total_hmeres;
+                        var pension_amount = 0;
+
+                        if (total_amount < 1000) {
+                            pension_amount = 100;
+                        }
+                        else if (total_amount < 5000) {
+                            pension_amount = 300;
+                        }
+                        else if (total_amount < 10000) {
+                            pension_amount = 500;
+                        }
+                        else if (total_amount < 20000) {
+                            pension_amount = 800;
+                        }
+                        else {
+                            pension_amount = 1000;
+                        }
+
+                        /* Get the modal */
+                        var modal = document.getElementById("myModal");
+
+                        /* Get the button that opens the modal */
+                        var btn = document.getElementById("calculate");
+
+                        /* Get the <span> element that closes the modal */
+                        var span = document.getElementsByClassName("close")[0];
+
+                        /* Open the modal */
+                        document.getElementById('amount').innerHTML = pension_amount;
+                        modal.style.display = "block";
+
+                        /* When the user clicks on <span> (x), close the modal */
+                        span.onclick = function() {
+                            modal.style.display = "none";
+                        }
+
+                        /* When the user clicks anywhere outside of the modal, close it */
+                        window.onclick = function(event) {
+                            if (event.target == modal) {
+                                modal.style.display = "none";
+                            }
+                        }
                     }
                 </script>
 
